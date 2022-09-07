f962a95da4316de16c26a303b021d29bceb66b95
Sebazt <juan.londono29@udea.edu.co>
2022-09-06 22:57:01 -0500

    finish javascript introduction

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-let firstName;
-let lastName;
-
-//console.log(`${firstName || 'Sebastian'} ${lastName || 'londoño'}`);
-
-const isActive = true;
-
-
-// con ternario
-const message = (!isActive === true)
-  ? 'Activo' 
-  : 'Inactivo chaval';
-
-console.log(message);
+console.log('Hola :D');
